5e2e3e73fcdefb38fc9bd0ae7581f97594859862
Daniel Ewert <ewert.daniel@gmail.com>
2016-11-10 22:08:31 +0100

    support for menu

--- js/braubuch.js
+++ js/braubuch.js
@@ -2,5 +2,11 @@
  * Created by daniel on 09.11.16.
  */
 $(document).ready(function(){
-    
+    $(".content").hide();
+    $("#recipe-form-display").show();
+
+    $("#menu a").click(function(event){
+        $(".content").hide();
+        $(event.target.hash).show();
+    });
 });
